433f8a3f849cf29fb331929834c1724d8e9e0b49
Karthick  Eswaran <karthickeswaran8@gmail.com>
2025-11-17 22:14:25 +0530

    correction in execution and balance calculation

--- src/execution.py
+++ src/execution.py
@@ -7,7 +7,7 @@ class Execution(Strategy):
         super().__init__()
         self.initial_balance = 100000
         self.lot_size = 1
-        self.balance = 0
+        self.balance =  self.initial_balance
         self.profit = 0
         self.loss = 0
         self.profit_trades = []
@@ -46,87 +46,84 @@ class Execution(Strategy):
         sl = None
 
         for i in range(len(df)):
-
-            if position == 0:
-                if df.iloc[i]['enter_long'] == True:
-                   
-                    entry_price = df.iloc[i]['Open']
-                    tp =  entry_price * (1 + 0.1)  # 2% Take Profit
-                    sl =  entry_price *  (1 - 0.05)  # 1% Stop Loss
+            close = df.iloc[i]['Close']
             
-                    df.at[df.index[i], 'buy_price'] = entry_price
-                    df.at[df.index[i], 'position'] = 1
-                    df.at[df.index[i], 'tp_long'] = tp
-                    df.at[df.index[i], 'sl_long'] = sl  
+            #Entry
+            if position == 0 and df.iloc[i]['enter_long']:    
+                entry_price = df.iloc[i]['Open']
+                tp =  entry_price *(1 + 0.08)
+                sl =  entry_price * (1 - 0.01)
+        
+                df.at[df.index[i], 'buy_price'] = entry_price
+                df.at[df.index[i], 'position'] = 1
+                df.at[df.index[i], 'tp_long'] = tp
+                df.at[df.index[i], 'sl_long'] = sl  
 
-                    position = 1
-                    continue
+                position = 1
+                continue
+            
+            #Exit
+            # if position == 1 and df.iloc[i]['exit_long']:
+            #     exit_price = close
+            #     df.at[df.index[i], 'sell_price'] = exit_price
+            #     df.at[df.index[i], 'position'] = 0
+            #     position = 0
 
+            #     pnl = (exit_price - entry_price) * self.lot_size
 
-            elif position == 1 and df.iloc[i]['exit_long'] == True:
-                exit_price = df.iloc[i]['Close']
+            #     if pnl >= 0:
+            #         self.profit_trades.append(pnl)
+            #     else:
+            #         self.loss_trades.append(abs(pnl))
+
+            #     self.balance += pnl
+
+            #     print(f"""-------------- EXIT LONG --------------
+            #         Entry Price: {entry_price}
+            #         Exit Price: {exit_price:.2f}
+            #         pnl: {pnl:.2f}
+            #         Balance: {self.balance:.2f}
+            #         """)
+            #     continue
+                
+            #Take Profit Hit
+            if position == 1 and close >= tp:
+                exit_price = tp
+                df.at[df.index[i], 'sell_price'] = exit_price
+                df.at[df.index[i], 'position'] = 0
+                position = 0
+                
+                pnl = (exit_price - entry_price) * self.lot_size
+                self.profit_trades.append(pnl)
+                self.balance += pnl
+               
+                print(f"""-------------- TP HIT --------------
+                    Entry Price: {entry_price}
+                    Exit Price: {exit_price:.2f}
+                    pnl: {pnl:.2f}
+                    Balance: {self.balance:.2f}
+                    """)
+                continue
+
+            #Stop Loss Hit
+            if position == 1 and close <= sl:
+
+                exit_price = sl
                 df.at[df.index[i], 'sell_price'] = exit_price
                 df.at[df.index[i], 'position'] = 0
                 position = 0
 
-                if exit_price <= sl:
-                    self.loss = self.balance - (entry_price - sl) * self.lot_size
-                    self.loss_trades.append(self.loss)
-                    self.balance  = self.loss
-                    print(f"""--------------  LOSS EXIT CONDITONS --------------
-                    Stop Loss hit: {sl:.2f}
+                pnl = (exit_price - entry_price) * self.lot_size
+                self.loss_trades.append(abs(pnl))
+                self.balance += pnl
+             
+                print(f"""-------------- SL HIT --------------
                     Entry Price: {entry_price}
-                    Loss: {self.loss:.2f}
+                    Exit Price: {exit_price:.2f}
+                    pnl: {pnl:.2f}
                     Balance: {self.balance:.2f}
                     """)
-                    continue
-                    
-
-                elif exit_price >= tp:
-                    self.profit = self.balance + (tp + entry_price) * self.lot_size
-                    self.profit_trades.append(self.profit)
-                    self.balance  = self.profit
-                    print(f"""--------------  PROFIT EXIT CONDITONS --------------
-                    Stop Loss hit: {tp:.2f}
-                    Entry Price: {entry_price}
-                    Loss: {self.profit:.2f}
-                    Balance: {self.balance:.2f}
-                    """)     
-                    continue      
-
-            elif position == 1 and df.iloc[i]['Close'] >= tp:
-                df.at[df.index[i], 'sell_price'] = tp
-                df.at[df.index[i], 'position'] = 0
-                position = 0
-                self.profit = self.balance + (tp + entry_price) * self.lot_size
-                self.profit_trades.append(self.profit)
-                self.balance  = self.profit
-            
-                print(f"""-------------- TP HIT --------------
-                Stop Loss hit: {tp:.2f}
-                Entry Price: {entry_price}
-                Profit: {self.profit:2f}
-                Balance: {self.balance:.2f}
-                """)           
                 continue
-
-            elif position == 1 and df.iloc[i]['Close'] <= sl:
-                df.at[df.index[i], 'sell_price'] = sl
-                df.at[df.index[i], 'position'] = 0
-                position = 0
-                self.loss = self.balance - (entry_price - sl) * self.lot_size
-                self.loss_trades.append(self.loss)
-                self.balance  = self.loss
-
-                print(f"""-------------- SL HIT --------------
-                Stop Loss hit: {sl:.2f}
-                Entry Price: {entry_price}
-                Loss: {self.loss:.2f}
-                Balance: {self.balance:.2f}
-                """)
-                continue
-        
-            continue
 
         self.results()
  
@@ -140,27 +137,24 @@ class Execution(Strategy):
         total_loss = sum(self.loss_trades)
         print(f"Total Loss: - {total_loss:.2f}")
 
-        pl = total_profit - total_loss
-        print(f"Profit - Loss: {pl:.2f}")
-
-        print(f"Balance: {self.initial_balance  + pl:.2f}")
+        npl =   total_profit - total_loss 
+        print(f"Profit - Loss: {npl:.2f}")
+        print(f"Balance: {self.initial_balance  + npl:.2f}")
 
         total_no_of_trades = self.profit_trades.__len__() +  self.loss_trades.__len__()
         win = (self.profit_trades.__len__()  / total_no_of_trades) * 100
         print(f"Win Rate %: {win:.2f}%")
 
-      
-        
         print("-------------------------------------------------")
         print(f"Total No Of Profit Trades: {self.profit_trades.__len__()}")
         print(f"Total No Of Loss Trades: {self.loss_trades.__len__()}")
         print("-------------------------------------------------")
-        for i in range(len(self.profit_trades)):
-            print(f" + {self.profit_trades[i]:.2f}")
+        for p in range(len(self.profit_trades)):
+            print(f" + {self.profit_trades[p]:.2f}")
         print("-------------------------------------------------")
-        for i in range(len(self.loss_trades)):
-            print(f" - {self.loss_trades[i]:.2f}")
+        for l in range(len(self.loss_trades)):
+            print(f" - {self.loss_trades[l]:.2f}")
        
 
-ex = Execution()
-ex.long_signal()
+# ex = Execution()
+# ex.long_signal()
